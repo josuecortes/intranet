554eda44dc8fe16f078fa202d2f581ce4c536f3c
Josue Cortes <josue.cort3s@gmail.com>
2017-01-31 17:25:20 -0300

    mexendo no user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,25 +20,18 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :etapas
   has_and_belongs_to_many :projetos
 
-  #has_attached_file :avatar, styles: { 
-  #  original: "800>600>", medium: "300x300>", thumb: "100x100#", info: "80x80#", chat: "128x128#" }, 
-  #  default_url: lambda { |image| ActionController::Base.helpers.asset_path('user-default.jpg') }
+  has_attached_file :avatar, styles: { 
+    original: "800>600>", medium: "300x300>", thumb: "100x100#", info: "80x80#", chat: "128x128#" }, 
+    default_url: lambda { |image| ActionController::Base.helpers.asset_path('user-default.jpg') }
 
-  #validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   #do_not_validate_attachment_file_type :avatar
   
   #validates_attachment_content_type :avatar, content_type: /\Aimage/
   #validates_attachment_file_name :avatar, matches: [/png\z/, /jpe?g\z/]
-  #do_not_validate_attachment_file_type :avatar
+  do_not_validate_attachment_file_type :avatar
 
-  has_attached_file :avatar,
-  :default_url => lambda { |image| ActionController::Base.helpers.asset_path('user-default.jpg') },
-  :default_style => :medium,
-  :use_timestamp => false,
-  :path => '/public/system/:class/:attachment/:id_partition/:style/:filename',
-  :styles => { original: "800>600>", medium: "300x300>", thumb: "100x100#", info: "80x80#", chat: "128x128#" }
-
-
+ 
 
   validates_presence_of :name, :cpf
 	validates_uniqueness_of :name, :cpf
